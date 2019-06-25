5d5e9264d0df82a85e4a9278a4b40bddec41a97c
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-25 16:48:37 -0500

    working on the login button

--- Header/header.js
+++ Header/header.js
@@ -1,23 +1,31 @@
-class Show{
-    constructor(event){
-        this.event = event;
-        this.button = document.querySelector('.dropdown-button');
-        console.log(this.button)
-        this.links = document.querySelector('.dropdown-links');
-        this.button.addEventListener('mouseover', ()=> {
-            this.showLinks();
-        })
-        this.event.addEventListener('click', ()=> {
-            this.hideLinks();
-        })
-    }
-    showLinks(){
-        this.links.style.display='flex';
-    }
-    hideLinks(){
-        this.links.style.display='none';
-    }
-}
+// class Show{
+//     constructor(event){
+//         this.event = event;
+//         this.button = document.querySelector('.dropdown-button');
+//         console.log(this.button)
+//         this.links = document.querySelector('.dropdown-links');
+//         this.button.addEventListener('click', ()=> {
+//             this.showLinks();
+//         })
+//         button.addEventListener('mouseout', ()=> {
+//             this.hideLinks();
+//         })
+//     }
+//     showLinks(){
+//         this.links.style.display==='flex'
+//     }
+//     hideLinks(){
+//         this.links.style.display='none';
+//     }
+// }
 
 
-let loginButton = document.querySelectorAll('.dropdown').forEach(event => new Show(event));
+// let loginButton = document.querySelectorAll('.dropdown').forEach(event => new Show(event));
+
+ 
+const dropdown = document.querySelectorAll('.dropdown-links')
+console.log(dropdown.classList)
+
+
+dropdown.addEventListener('click', event => dropdownLinks.style.display = 'block') 
+
