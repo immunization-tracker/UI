bc19af96434bd93788f1b278b34aca4ebc24e532
Arvin Agas <arvagas@gmail.com>
2019-06-26 11:57:39 -0400

    carousel still wip

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -4,30 +4,49 @@ class Carousel {
         this.firstImg = caro.querySelector('img')
 
         this.firstImg.classList.add('img-reveal')
+
+        if (counter >= 10000) {
+
+        }
+        else if (counter < 10000) {
+            clearInterval(countUp)
+            startTime = Date.now()
+            countUp = setInterval(timer, 1) // Updates every 1ms
+        }
     }
 
-    showImg(param) {
-        const images = document.querySelectorAll('.carousel img')
-        const curIndex = index
+    // showImg(param) {
+    //     const images = document.querySelectorAll('.carousel img')
+    //     const curIndex = index
 
-        if ((index+param>=0) && (index+param<=2)) {
-            index += param
-        } else if (index+param>2) {
-            index = 0
-        } else if (index+param<0) {
-            index = 2
-        }
+    //     if ((index+param>=0) && (index+param<=2)) {
+    //         index += param
+    //     } else if (index+param>2) {
+    //         index = 0
+    //     } else if (index+param<0) {
+    //         index = 2
+    //     }
 
-        images.forEach(img => img.classList.remove('img-reveal'))
+    //     images.forEach(img => img.classList.remove('img-reveal'))
 
-        if (curIndex<index) {
-            images[index].classList.add('img-reveal')
-        } else {
-            images[index].classList.add('img-reveal')
-        }
-        
+    //     if (curIndex<index) {
+    //         images[index].classList.add('img-reveal')
+    //     } else {
+    //         images[index].classList.add('img-reveal')
+    //     }
+    // }
+}
+
+let index=0
+let startTime
+let countUp
+let counter
+
+let timer = () => {
+    counter = Date.now() - startTime
+    if (counter >= 10000) {
+        clearInterval(countUp)
     }
 }
 
-let index = 0
 let carousel = new Carousel(document.querySelector('.carousel'))
