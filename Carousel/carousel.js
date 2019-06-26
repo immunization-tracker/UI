fc7ef33eb4b30ea5a89c63197e4df644f7693941
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-26 14:42:41 -0500

    pulled from jasons branch

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -1,56 +1,56 @@
-class Carousel {
-    constructor(caro){
-        this.caro = caro
-        this.firstImg = caro.querySelector('img')
+// class Carousel {
+//     constructor(caro){
+//         this.caro = caro
+//         this.firstImg = caro.querySelector('img')
 
-        this.firstImg.classList.add('img-reveal')
-        // startTime = Date.now()
-        // if (counter >= 10000) {
+//         this.firstImg.classList.add('img-reveal')
+//         // startTime = Date.now()
+//         // if (counter >= 10000) {
 
-        //     if (index < 2) {
-        //         index++
-        //     } else {
-        //         index=0
-        //     }
+//         //     if (index < 2) {
+//         //         index++
+//         //     } else {
+//         //         index=0
+//         //     }
 
-        //     images.forEach(img => img.classList.remove('img-reveal'))
-        //     images[index].classList.add('img-reveal')
-        //     startTime = Date.now()
-        //     countUp = setInterval(timer, 1)
-    }
+//         //     images.forEach(img => img.classList.remove('img-reveal'))
+//         //     images[index].classList.add('img-reveal')
+//         //     startTime = Date.now()
+//         //     countUp = setInterval(timer, 1)
+//     }
 
-    showImg(param) {
-        const images = document.querySelectorAll('.carousel img')
-        const curIndex = index
+//     showImg(param) {
+//         const images = document.querySelectorAll('.carousel img')
+//         const curIndex = index
 
-        if ((index+param>=0) && (index+param<=2)) {
-            index += param
-        } else if (index+param>2) {
-            index = 0
-        } else if (index+param<0) {
-            index = 2
-        }
+//         if ((index+param>=0) && (index+param<=2)) {
+//             index += param
+//         } else if (index+param>2) {
+//             index = 0
+//         } else if (index+param<0) {
+//             index = 2
+//         }
 
-        images.forEach(img => img.classList.remove('img-reveal'))
+//         images.forEach(img => img.classList.remove('img-reveal'))
 
-        if (curIndex<index) {
-            images[index].classList.add('img-reveal')
-        } else {
-            images[index].classList.add('img-reveal')
-        }
-    }
-}
+//         if (curIndex<index) {
+//             images[index].classList.add('img-reveal')
+//         } else {
+//             images[index].classList.add('img-reveal')
+//         }
+//     }
+// }
 
-let index=0
-let startTime
-let countUp
-let counter
+// let index=0
+// let startTime
+// let countUp
+// let counter
 
-let timer = () => {
-    counter = Date.now() - startTime
-    if (counter >= 10000) {
-        clearInterval(countUp)
-    }
-}
+// let timer = () => {
+//     counter = Date.now() - startTime
+//     if (counter >= 10000) {
+//         clearInterval(countUp)
+//     }
+// }
 
-let carousel = new Carousel(document.querySelector('.carousel'))
+// let carousel = new Carousel(document.querySelector('.carousel'))
