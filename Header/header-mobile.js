a5f09c741c8271dd56139c4ed058537cb9df68d0
Arvin Agas <arvagas@gmail.com>
2019-06-27 02:06:07 -0400

    restyled navbar, added more classes for navbar, container edits

--- Header/header-mobile.js
+++ Header/header-mobile.js
@@ -1,5 +1,16 @@
+class Mobile{
+    constructor(event){
+        this.event = event;
+        this.button = document.querySelector('.button-mobile p');
+        this.links = document.querySelectorAll('.page-links');
+        this.button.addEventListener('click', ()=> {
+            this.showMobileMenu();
+        })
 
-// const mobileButton = document.querySelector('.button-mobile');
-// const nav = document.querySelectorAll('nav a');
+    }
+    showMobileMenu(){
+        this.links.forEach(link => link.classList.toggle('reveal-page-links'))
+    }
+}
 
-// mobileButton.addEventListener('click')
+let mobileButton = document.querySelectorAll('.button-mobile').forEach(event => new Mobile(event));
