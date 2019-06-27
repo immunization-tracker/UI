257a20046b0b61270f92fb6438d4a99a46d17c47
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-26 19:25:37 -0500

    fixed the bugs on the tablet view

--- Header/header-mobile.js
+++ Header/header-mobile.js
@@ -1,16 +1,5 @@
-class MenuMobile{
-    constructor(event){
-        this.event = event;
-        console.log(this.event)
-        this.button = document.querySelector('.button-mobile');
-        this.menu = document.querySelector('.show-menu');
-        this.button.addEventListener('click', ()=> {
-            this.showMenu();
-        })
-    }
-    showMenu(){
-        this.menu.classList.toggle('hidden-menu');
-    }
-}
 
-let menuBar = document.querySelectorAll('header').forEach(event => new MenuMobile(event));
+// const mobileButton = document.querySelector('.button-mobile');
+// const nav = document.querySelectorAll('nav a');
+
+// mobileButton.addEventListener('click')
