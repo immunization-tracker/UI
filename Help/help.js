eacd379306df5b2ba0fc3b1b51b8a8aa40c8e4f0
Arvin Agas <arvagas@gmail.com>
2019-06-28 07:52:25 -0400

    most of the help page styling complete

--- Help/help.js
+++ Help/help.js
@@ -15,7 +15,7 @@ class Accordion {
     collapse() {
         if (this.autoClose) {
             const currentPanel = this.accordion.querySelector('.panel-content.toggle-on')
-            if (currentPanel) currentPanel.classList.remove('toggle-on')      
+            if (currentPanel) currentPanel.classList.remove('toggle-on')
         }
     }
 }
@@ -26,12 +26,23 @@ class Panel {
         this.openButton = panel.querySelector('.panel-btn-open')
         this.closeButton = panel.querySelector('.panel-btn-close')
         this.panelContent = panel.querySelector('.panel-content')
-        this.openButton.addEventListener('click', () => this.togglePanel())
+        this.panelBar = panel.querySelector('.panel-bar')
+
+        this.panelBar.addEventListener('click', () => this.togglePanel())
+
+        document.addEventListener('click', event => {
+            if (event.target.closest('.accordion')) return
+            this.openButton.classList.remove('hide-btn')
+            this.closeButton.classList.add('hide-btn')
+        })
+
     }
     
     togglePanel() {
         this.parentAccordion.collapse()
         this.panelContent.classList.toggle('toggle-on')
+        this.openButton.classList.toggle('hide-btn')
+        this.closeButton.classList.toggle('hide-btn')
     }
 }
     
