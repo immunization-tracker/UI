43d4083f81d5d12d76c921ecf68cf4e1a6514e16
JCRN <clemons.jason@gmail.com>
2019-06-28 00:13:15 -0700

    wip: contact pg - continued work on modal sub class, added icons and constructed modal content object

--- Contact/contact.js
+++ Contact/contact.js
@@ -4,13 +4,18 @@ const btnAttrs = {
     'border-style': 'none',
     'border-radius': '50px',
     color: 'white',
-    backgroundColor: '#5d348b',
+    backgroundColor: '#32006e',
     padding: '15px 25px'
 }
 
 const modalContent = {
     'support': {
-        
+        img: '../Images/Contact/contact.svg',
+        alt: 'sillouhetted icon of a nurse',
+        h1: 'CUSTOMER SUPPORT',
+        p1: 'Thank you for using Immunization Tracker! <br> Please contact customer support:'
+        p2: 
+
     }
 }
 
@@ -28,6 +33,9 @@ class Button {
         // Assign this.button to the element passed into the constructor
         this.button = button
         
+        // Add numbered id attribute to this.button by calling counter function
+        this.button.setAttribute('id', `btn-${counter()}`)
+        
         // get / set button style attributes from the 'btnAttrs' object   
         button.style.width = btnAttrs.width
         button.style.fontSize = btnAttrs.fontSize
@@ -37,14 +45,14 @@ class Button {
         button.style.backgroundColor = btnAttrs.backgroundColor
         button.style.padding = btnAttrs.padding
 
-        // Add numbered id attribute to this.button by calling counter function
-        this.button.setAttribute('id', `btn-${counter()}`) 
+        // Create a new instance of Modal class
+        this.modal = new Modal(this.modal)        
         
         // Click event handler, calls modal() 
-        button.addEventListener('click', event => this.modal())
+        button.addEventListener('click', event => this.select())
     }
 
-    modal() {
+    select() {
         // Get DOM elements with class '.btn' and store them in 'buttons'
         const buttons = document.querySelectorAll('.btn')
 
@@ -52,9 +60,39 @@ class Button {
         buttons.forEach(button => button.classList.remove('btn-clicked'))
 
         // Adds the class 'btn-clicked' attribute to the this.button element
-        this.button.classList.add('btn-clicked')        
+        this.button.classList.add('btn-clicked')
+
+        // Get button id
+        const clicked = this.button.id 
+
+        // Evaluate button id and call this.modal.select() passing the id value
+        (clicked === 1) ? this.modal.select(1) : (clicked === 2) ? this.modal.select(2) : (clicked === 3) ? this.modal.select(3) : false
+        
+        
+        }
     }
 
+
+class Modal {
+    constructor(modal) {
+        // Assign this.modal to passed in element
+        this.modal = modal
+    }
+
+    select(num) {
+       switch(num) {
+            case 1: 
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+    }
 }
 
 
