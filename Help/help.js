284346e40078681ab597fd3b5f4014779e928b22
Arvin Agas <arvagas@gmail.com>
2019-06-29 15:50:50 -0400

    fixed accordion collapsing when panel is clicked while another one is open, closes other panel: check when panel is clicked while it is already open, close clicked panel: check

--- Help/help.js
+++ Help/help.js
@@ -3,9 +3,22 @@ const expandBtn = document.querySelectorAll('.expand-btn')
 const collapseBtn = document.querySelectorAll('.collapse-btn')
 
 $(".accordion").on("click", ".accordion-header", function() {
-    $(this).toggleClass("active-header").next().slideToggle()
-    $(this).find('.expand-btn').toggleClass('hidden-btn')
-    $(this).find('.collapse-btn').toggleClass('hidden-btn')
+
+    // if border is highlighted when clicked, close the panel and do nothing else
+    if ($(this).hasClass('active-header')) {
+        $(this).toggleClass("active-header").next().slideToggle()
+        $(this).find('.expand-btn').toggleClass('hidden-btn')
+        $(this).find('.collapse-btn').toggleClass('hidden-btn')
+    } else { // otherwise, reset the other panels (collapse all) and then open clicked panel
+        // resets all panels
+        $('.accordion-header').removeClass("active-header").next().slideUp()
+        expandBtn.forEach(item => item.classList.remove('hidden-btn'))
+        collapseBtn.forEach(item => item.classList.add('hidden-btn'))
+        // turn on panel
+        $(this).toggleClass("active-header").next().slideToggle()
+        $(this).find('.expand-btn').toggleClass('hidden-btn')
+        $(this).find('.collapse-btn').toggleClass('hidden-btn')
+    }
 })
 
 document.addEventListener('click', event => {
