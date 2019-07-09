d529e4832eaccf50551a5b75813d1da2fc031775
JCRN <clemons.jason@gmail.com>
2019-07-09 08:01:16 -0700

    wip: dynamic modal functioning as intended

--- Contact/contact.js
+++ Contact/contact.js
@@ -20,25 +20,16 @@ const modalContentObj = {
 
     header: {
         contact: 'CONTACT US',
-        cs: 'CUSTOMER SUPPORT SERVICES',
-        ps: 'PROVIDER SUPPORT SERVICES'
+        cs: 'CUSTOMER SUPPORT',
+        ps: 'PROVIDER SUPPORT'
     },
 
     content: {
-        contact: `Thank you for using Immunization Tracker! Please feel free to reach out to us with any comments, questions, or concerns.`,
-        cs: `Immunization Tracker Customer Support representatives are available on the following days and times: `, 
-        ps: `Immunization Tracker Provider Support representatives are available on the following days and times: `,
-        address: `CORPORATE HEADQUARTERS
-                  Immunization Tracker Services Inc.
-                  1973 42nd Ave West 
-                  Seattle, WA 98109`,
-        genInfo: `Toll-Free: 888-555-1111\nPhone: 206-555-1212\nEmail: info@immunizationtracker.net`,
-        csPhn: `CUSTOMER SUPPORT SERVICES
-                Phone: 888-555-1212
-                Monday - Friday 8:00 AM - 9:00 PM PST`,
-        psPhn: `PROVIDER SUPPORT SERVICES
-                Phone: 800-555-2323
-                Monday - Friday 8:00 AM - 9:00 PM PST`,    
+        contact: `Thank you for using Immunization Tracker!\nPlease feel free to reach out to us with any comments, questions, or concerns.`,
+        cs: `Representatives are available:\n\nMonday - Friday 8:00 AM - 9:00 PM PST`, 
+        genInfo: `Toll-Free: 888-555-1111\nPhone: 206-555-1212\nEmail: info@immunizationtracker.net\n\nITS Inc.\n1973 42nd Ave West\nSeattle, WA 98109`,
+        csPhn: `Toll Free: 888-555-1212`,
+        psPhn: `Toll Free: 800-555-2323`,    
     } 
 }
 
@@ -86,7 +77,12 @@ const buttons = document.querySelectorAll('.btn')
 // Create a new Button object for each element in buttons
 buttons.forEach(button => {
     new Button(button);
-    button.addEventListener('click', event => toggleModal());
+    button.addEventListener('click', event => {
+        let ele = event.target;        
+        let btnId = ele.id;
+        buidlModal(btnId);
+        toggleModal();
+    });
 })
 
 
@@ -98,36 +94,12 @@ buttons.forEach(button => {
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.close-button');
 const modalHeader = document.querySelector('.modal-header');
+const modalContent = document.querySelector('.modal-content');
 
 // Create header and paragraph elements
 const eleH1= document.createElement('H1');
 const eleP1 = document.createElement('P');
 const eleP2 = document.createElement('P');
-
-// Get content
-let modal_HeadLine = modalContentObj['header']['contact'];
-let modal_Paragraph1 = modalContentObj['content']['contact']
-let modal_Paragraph2 = modalContentObj['content']['genInfo']
-
-// // Set content to text nodes
-// const txtH1 = document.createTextNode(modal_HeadLine);
-// const txtP1 = document.createTextNode(modal_Paragraph1);
-// const txtP2 = document.createTextNode(modal_Paragraph2);
-
-// Append text nodes to elements
-// eleH1.appendChild(txtH1);
-// eleP1.appendChild(txtP1);
-// eleP2.appendChild(txtP2);
-
-eleH1.innerHTML = modal_HeadLine;
-eleP1.innerHTML = modal_Paragraph1;
-eleP2.innerHTML = modal_Paragraph2;
-
-// Append elements to modal header 
-modalHeader.appendChild(eleH1);
-modalHeader.appendChild(eleP1);
-modalHeader.appendChild(eleP2);
-
 
 // Event handlers ************************ 
 
@@ -137,6 +109,43 @@ window.addEventListener("click", event => windowOnClick(event));
 
 
 // Functions *****************************
+
+// Build modal
+function buidlModal(id) {
+    // Determine content
+    let headLine = "";
+    let para1 = "";
+    let para2 = "";
+   
+    if(id === 'btn-1') {
+        headLine = 'contact';
+        para1 = 'contact';
+        para2 = 'genInfo';
+    } else if(id === 'btn-2') {
+        headLine = 'cs';
+        para1 = 'csPhn';
+        para2 = 'cs';
+    } else {
+        headLine = 'ps';
+        para1 = 'psPhn';
+        para2 = 'cs'
+    }
+
+    // Get modal content
+    let modal_HeadLine = modalContentObj['header'][headLine];
+    let modal_Paragraph1 = modalContentObj['content'][para1];
+    let modal_Paragraph2 = modalContentObj['content'][para2];
+
+    // Set content
+    eleH1.innerHTML = modal_HeadLine;
+    eleP1.innerHTML = modal_Paragraph1;
+    eleP2.innerHTML = modal_Paragraph2;
+
+    // Append elements to modal header 
+    modalHeader.appendChild(eleH1);
+    modalHeader.appendChild(eleP1);
+    modalHeader.appendChild(eleP2);
+}
 
 // Toggle '.show-modal' in modal
 function toggleModal() {
